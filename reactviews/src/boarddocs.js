9b78127224219ec5875d449b24ca585a4aedbe28
Yves AGOSTINI <yves.agostini@univ-lorraine.fr>
2018-06-28 15:18:27 +0200

    react views: fix redirect with undoable board

--- reactviews/src/boarddocs.js
+++ reactviews/src/boarddocs.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Edit, SimpleForm  } from 'react-admin';
 import RichTextInput from 'ra-input-rich-text';
+import Divider from '@material-ui/core/Divider';
 
 export const BoardEdit = (props) => (
-    <Edit title="Docs" {...props}>
-        <SimpleForm redirect="show">
+    <Edit title="Docs" undoable={false} {...props}>
+        <SimpleForm redirect="/">
             <RichTextInput
                 source="docs"
                 label="Main Doc"
             />
+            <br />
+            <br />
+            <Divider inset />
             <RichTextInput
                 source="docagents"
                 label="Agents"
             />
+            <br />
+            <br />
+            <Divider inset />
             <RichTextInput
                 source="docsearchs"
                 label="Searchs"
