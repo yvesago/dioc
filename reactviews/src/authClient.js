32c0ddd7d1e5217997ee8eb9dde59fe0027a2631
Yves AGOSTINI <yves.agostini@univ-lorraine.fr>
2024-03-14 17:07:22 +0100

    new authclient with callback, fix token name

--- reactviews/src/authClient.js
+++ reactviews/src/authClient.js
@@ -1,29 +1,43 @@
 import decodeJwt from 'jwt-decode';
 import request from 'superagent';
+import { MyConfig } from './MyConfig';
 
 const authClient = (authUrl) => ({
-    login: () => {return request.get(authUrl);},
-    logout: () => {
-        localStorage.removeItem('token');
+    login: async () => {console.log('login'); return request.get(authUrl);},
+    logout: async () => {
+        localStorage.removeItem('gotoken');
         return Promise.resolve();
     },
-    checkAuth: () => Promise.resolve(),
-    checkError: (error) => {
+    checkAuth: async () => {console.log('checkAuth');
+        const decodedToken = decodeJwt(localStorage.getItem('gotoken'));
+        console.log('== getIdentity() : ' + JSON.stringify({ id: decodedToken.id, fullName: decodedToken.id, avatar: '' }) );
+        return Promise.resolve();},
+    checkError: async (error) => {
         const status = error.status;
         if (status === 401 || status === 403) {
-            localStorage.removeItem('token');
+            localStorage.removeItem('gotoken');
             return Promise.reject();
         }
         return Promise.resolve();
     },
     getPermissions: params => Promise.resolve(),
-    getIdentity: () => {
-        if ( localStorage.getItem('token') !== null ) {
-            const decodedToken = decodeJwt(localStorage.getItem('token'));
+    getIdentity: async () => {
+        console.log('**getIdentity**');
+        if ( localStorage.getItem('gotoken') !== null ) {
+            const decodedToken = decodeJwt(localStorage.getItem('gotoken'));
             //console.log('== getIdentity() : ' + JSON.stringify({ id: decodedToken.id, fullName: decodedToken.id, avatar: '' }) );
             return { id: decodedToken.id, fullName: decodedToken.id, avatar: '' };
         }
         return { id: '', fullName: '', avatar: ''};
+    },
+    handleCallback: async () => {
+        console.log('**handleCallback**');
+        var match = window.location.href.match(/\?(.*)$/);
+        console.log(match[1]);
+        const token = match[1]; 
+        localStorage.setItem('gotoken', token);
+        window.location.href = MyConfig.BASE_PATH;
+        return Promise.resolve();
     },
 });
 
